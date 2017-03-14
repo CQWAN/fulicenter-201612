c535598c91e6e958dd938f20e2dd82415bd95aca
lpp <2107467756@qq.com>
2017-03-14 17:57:49 +0800

    项目搭建

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -1,15 +1,50 @@
 package cn.ucai.fulicenter.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.ucai.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rbNewGoods)
+    RadioButton mrbNewGoods;
+    @BindView(R.id.rbBoutique)
+    RadioButton mrbBoutique;
+    @BindView(R.id.rbCategory)
+    RadioButton mrbCategory;
+    @BindView(R.id.rbCart)
+    RadioButton mrbCart;
+    @BindView(R.id.rbContact)
+    RadioButton mrbContact;
+    @BindView(R.id.rgMenu)
+    RadioGroup mrgMenu;
+    @BindView(R.id.layout_content)
+    FrameLayout mlayoutContent;
+    Unbinder bind;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bind = ButterKnife.bind(this);
+    }
+
+    public void onCheckedChange(View view) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bind.unbind();
     }
 }
