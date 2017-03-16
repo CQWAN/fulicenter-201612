b2ed84ebd4819772f5b311dce4782a1b47412863
lpp <2107467756@qq.com>
2017-03-16 21:44:38 +0800

    设置分类首页适配器

--- app/src/main/java/cn/ucai/fulicenter/ui/adapter/CategoryAdatper.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/adapter/CategoryAdatper.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
+import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.model.bean.CategoryChildBean;
 import cn.ucai.fulicenter.model.bean.CategoryGroupBean;
+import cn.ucai.fulicenter.model.utils.ImageLoader;
 
 /**
  * Created by LPP on 2017/3/16.
@@ -62,17 +66,57 @@ public class CategoryAdatper extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-
-        return null;
+        CategoryGroupBean group = getGroup(groupPosition);
+        GroupHolder groupHolder = null;
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.item_category_group, null);
+            groupHolder = new GroupHolder();
+            groupHolder.ivCategoryGroup = (ImageView) convertView.findViewById(R.id.ivGroupImage);
+            groupHolder.tvCategoryGroup = (TextView) convertView.findViewById(R.id.tvGroupText);
+            groupHolder.ivIsExpand = (ImageView) convertView.findViewById(R.id.ivIsExpand);
+            convertView.setTag(groupHolder);
+        } else {
+            groupHolder = (GroupHolder) convertView.getTag();
+        }
+        ImageLoader.downloadImg(context,groupHolder.ivCategoryGroup,group.getImageUrl());
+        groupHolder.tvCategoryGroup.setText(group.getName());
+        if (isExpanded) {
+            groupHolder.ivIsExpand.setImageResource(R.mipmap.expand_off);
+        } else {
+            groupHolder.ivIsExpand.setImageResource(R.mipmap.expand_on);
+        }
+        return convertView;
+    }
+    class GroupHolder{
+        ImageView ivCategoryGroup;
+        TextView tvCategoryGroup;
+        ImageView ivIsExpand;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        CategoryChildBean child = categoryChildList.get(groupPosition).get(childPosition);
+        ChildHolder childHolder = null;
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.item_category_child, null);
+            childHolder = new ChildHolder();
+            childHolder.ivCategoryChild = (ImageView) convertView.findViewById(R.id.ivChildImage);
+            childHolder.tvCategoryChild = (TextView) convertView.findViewById(R.id.tvChildText);
+            convertView.setTag(childHolder);
+        } else {
+            childHolder = (ChildHolder) convertView.getTag();
+        }
+        ImageLoader.downloadImg(context,childHolder.ivCategoryChild,child.getImageUrl());
+        childHolder.tvCategoryChild.setText(child.getName());
+        return convertView;
+    }
+    class ChildHolder{
+        ImageView ivCategoryChild;
+        TextView tvCategoryChild;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+        return true;
     }
 }
