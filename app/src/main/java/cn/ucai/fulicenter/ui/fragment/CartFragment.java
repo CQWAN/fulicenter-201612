3748fbe7e38f927e29bc176f7b4eb2dec375814f
lpp <2107467756@qq.com>
2017-03-20 20:54:11 +0800

    添加首选项保存用户名

--- app/src/main/java/cn/ucai/fulicenter/ui/fragment/CartFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/fragment/CartFragment.java
@@ -1,6 +1,10 @@
 package cn.ucai.fulicenter.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import cn.ucai.fulicenter.R;
 
@@ -8,11 +12,15 @@ import cn.ucai.fulicenter.R;
  * Created by LPP on 2017/3/20.
  */
 public class CartFragment extends BaseFragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = View.inflate(getActivity(), R.layout.fragment_cart, null);
+        return layout;
+    }
+
     @Override
     protected void initView() {
-        View layout = View.inflate(getActivity(), R.layout.fragment_cart, null);
-
-
     }
 
     @Override
--- app/src/main/java/cn/ucai/fulicenter/ui/fragment/PersonalCenterFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/fragment/PersonalCenterFragment.java
@@ -1,6 +1,10 @@
 package cn.ucai.fulicenter.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import cn.ucai.fulicenter.R;
 
@@ -9,10 +13,15 @@ import cn.ucai.fulicenter.R;
  */
 
 public class PersonalCenterFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = View.inflate(getActivity(), R.layout.fragment_personal_center, null);
+        return layout;
+    }
 
     @Override
     protected void initView() {
-        View layout = View.inflate(getActivity(), R.layout.fragment_personal_center, null);
     }
 
     @Override
