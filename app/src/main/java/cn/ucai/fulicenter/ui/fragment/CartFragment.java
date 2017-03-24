e5ee4f70200a85b1951e43abb0fd61bf1a9ba511
lpp <2107467756@qq.com>
2017-03-24 11:04:58 +0800

    动态更新收藏Activity

--- app/src/main/java/cn/ucai/fulicenter/ui/fragment/CartFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/fragment/CartFragment.java
@@ -2,34 +2,99 @@ package cn.ucai.fulicenter.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.model.bean.CartBean;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.NetDao;
+import cn.ucai.fulicenter.model.utils.OkHttpUtils;
+import cn.ucai.fulicenter.ui.activity.MainActivity;
+import cn.ucai.fulicenter.ui.view.SpaceItemDecoration;
 
 /**
  * Created by LPP on 2017/3/20.
  */
-public class CartFragment extends BaseFragment{
-    @Nullable
+public class CartFragment extends BaseFragment {
+    private static final String TAG = CartFragment.class.getSimpleName();
+    @BindView(R.id.tv_refresh)
+    MainActivity mContext;
+    ArrayList<CartBean> mGoodsList;
+    LinearLayoutManager mLayoutManager;
+    int pageId;
+
+    @BindView(R.id.tv_cart_sum_price)
+    TextView mTvCartSumPrice;
+    @BindView(R.id.tv_cart_save_price)
+    TextView mTvCartSavePrice;
+    @BindView(R.id.tv_refresh)
+    TextView tvRefresh;
+    @BindView(R.id.tv_nothing)
+    TextView tvNothing;
+    @BindView(R.id.rv)
+    RecyclerView mRv;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout mSrl;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = View.inflate(getActivity(), R.layout.fragment_cart, null);
+        ButterKnife.bind(this, layout);
+        mContext = (MainActivity) getActivity();
         return layout;
     }
 
     @Override
     protected void initView() {
+        mSrl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        mLayoutManager = new LinearLayoutManager(mContext);
+        mRv.setLayoutManager(mLayoutManager);
+        mRv.setHasFixedSize(true);
+//        mRv.setAdapter(mAdapter);
+        mRv.addItemDecoration(new SpaceItemDecoration(12));
+//        setCartLayout(false);
     }
 
     @Override
     protected void initData() {
+        User user = FuLiCenterApplication.getUser();
+        NetDao.downloadCart(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String result) {
 
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
     }
 
     @Override
     protected void setListener() {
+
+    }
+
+    @OnClick(R.id.tv_cart_buy)
+    public void total() {
 
     }
 }
